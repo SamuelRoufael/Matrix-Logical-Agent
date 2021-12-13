259d2e7bea652d3aa0bb4e6962116aa60239d1fb
SamuelRoufael <SamuelRoufael@users.noreply.github.com>
2021-12-13 21:00:28 +0200

    Add Comments

--- Matrix.pl
+++ Matrix.pl
@@ -1,6 +1,7 @@
 :-include("KB.pl").
 
-% ---------------------------------------------------------------------------
+% --------------------- isBorder(NeoLoc) -----------------------------------------
+% True only if the passed location is outside the borders of the grids.
 
 isBorder([X,Y]) :- 
 	grid(_,Y); 
@@ -8,14 +9,18 @@ isBorder([X,Y]) :-
 	X < 0;
 	Y < 0.
 
-% ---------------------------------------------------------------------------
+% ---------------------- move(Action, PrevLoc, NewLoc) ----------------------
+% Takes the direction which neo will move to, the current location, and the 
+% new location after applying the move action on the current location.
 
 move(up,[X,Y],[X1,Y]) :- X1 is X - 1.
 move(down,[X,Y],[X1,Y]) :- X1 is X + 1.
 move(left,[X,Y],[X,Y1]) :- Y1 is Y - 1.
 move(right,[X,Y],[X,Y1]) :- Y1 is Y + 1.
 
-% ---------------------------------------------------------------------------
+% -------------------------- atBooth(NeoLoc) --------------------------------
+% true only if the passed current neo's location is equal to that of the telephone
+% booth in the knowledge base.
 
 atBooth([NeoX, NeoY]) :-
 	booth(NeoX,NeoY).
@@ -28,9 +33,12 @@ atBooth([NeoX, NeoY]) :-
 isGoal(N, C, []) :-
 	atBooth(N),
 	capacity(C).
-	
-% -------------------------- Successor State Axioms --------------------------
 
+% ---------------------------------------------------------------------------	
+% -------------------------- Successor State Axioms -------------------------
+% ---------------------------------------------------------------------------
+
+% ------------------------------ Initial State ------------------------------
 solve([NeoX, NeoY], Capacity, Hostages, s0) :-
 	capacity(Capacity),
 	neo_loc(NeoX, NeoY),
@@ -76,17 +84,24 @@ solve(Neo, Capacity, Hostages, result(Action, State)) :-
 	% Stay in current State
 	(
 		solve(Neo, Capacity, Hostages, State),
+		
+		% --------------- Action is carry, and neo is not allowed to carry any hostages ---------------
 		(Action = carry -> (Capacity = 0; \+ member(Neo, Hostages))),
+		
+		% ---------- Action is drop, and neo is not carrying any hostages at the given state ----------
 		(Action = drop -> capacity(Capacity)),
+		
+		% -------- Action is a move action, and the new location is outside the grid's borders --------
 		((Action \= drop, Action \= carry) -> move(Action, Neo, NewNeo), isBorder(NewNeo))
-
 	).
+
+% -----------------------------------------------------------------------------
 	
 goalHelper1(_, _, R):- R \= depth_limit_exceeded.
 
 goalHelper1(Limit, S, depth_limit_exceeded):-
 	call_with_depth_limit(once(goalHelper2(S)),Limit,R),
-	New_Limit is Limit + 10,
+	New_Limit is Limit + 1,
 	goalHelper1(New_Limit,S,R).
   
 goalHelper2(State):-
